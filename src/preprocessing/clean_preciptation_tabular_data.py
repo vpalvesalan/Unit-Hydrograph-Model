cf94caf2870eddcb560cdce7c2261ef8c2ebabef
Alan Alves <vpalvesalan@gmail.com>
2025-02-10 14:23:22 -0300

    Implement function

---
+++ src/preprocessing/clean_preciptation_tabular_data.py
@@ -0,0 +1,58 @@
+import pandas as pd
+
+def unpivot_v2(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Transforms a v2 precipitation wide-format DataFrame into a long-format DataFrame by unpivoting columns, 
+    cleaning the data, and restructuring it for easier analysis.
+
+    The function performs the following steps:
+    1. Drops unnecessary columns.
+    2. Melts the DataFrame to long format based on the 'DATE' column.
+    3. Combines 'DATE' and 'Time_Attribute' for a new timestamp.
+    4. Extracts 'Attribute' from the 'Time_Attribute' column.
+    5. Pivots the data to separate out different measurements.
+    6. Renames the columns for clarity.
+
+    Args:
+    -----
+    df (pd.DataFrame): The input DataFrame to be unpivoted.
+
+    Returns:
+    --------
+    pd.DataFrame: The transformed long-format DataFrame with measurements separated by attribute.
+    """
+
+    columns_to_trop = ['STATION', 'ELEMENT','LATITUDE', 'LONGITUDE', 'ELEVATION', 'DlySum', 'DlySumMF', 'DlySumQF', 'DlySumS1', 'DlySumS2']
+    df = df.drop(columns=columns_to_trop)
+
+    # Identifying columns to melt
+    id_vars = ['DATE']
+    value_vars = [col for col in df.columns if col not in id_vars]
+
+    # Melting to long format
+    df_long = df.melt(id_vars=id_vars,
+                                        value_vars=value_vars,
+                                        var_name='Time_Attribute',
+                                        value_name='Value')
+
+    # Add Time to DATE column and extract Attribute from 'Time_Attribute'
+    df_long['DATE'] = df_long['DATE'].astype(str) + ' ' + df_long['Time_Attribute'].str[:4]
+    df_long['Attribute'] = df_long['Time_Attribute'].str[4:]
+
+    df_long = df_long.drop(columns=['Time_Attribute'])
+
+    # Pivoting to separate Value, Measurement Flag, Quality Flag, and Source Flags
+    df_final = df_long.pivot_table(
+        index=['DATE'], 
+        columns='Attribute', 
+        values='Value', 
+        aggfunc='first'
+    ).reset_index()
+
+    df_final.columns.name = None 
+    df_final = df_final.rename({
+        'DATE':'date',
+        'Val':'height'
+    }, axis='columns')
+    
+    df_final
